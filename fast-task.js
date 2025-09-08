bc7ab91c26f2c602871f48a53270f8231919f810
Al Amin <alamin.dev01@gmail.com>
2025-09-08 18:59:28 +0600

    added html file

--- fast-task.js
+++ fast-task.js
@@ -1,13 +1,20 @@
-var apple = 370;
-var orange = 330;
-var total = 1000;
-var appleOrange = apple + orange;
-var merchantBackToMe = total - appleOrange
-console.log(apple)
-console.log(orange)
-console.log(total)
-console.log(appleOrange)
-console.log(merchantBackToMe)
-// console.log(total - appleOrange)
-// console.log(apple + orange)
+// var apple = 370;
+// var orange = 330;
+// var total = 1000;
+// var appleOrange = apple + orange;
+// var merchantBackToMe = total - appleOrange
+// console.log(apple)
+// console.log(orange)
+// console.log(total)
+// console.log(appleOrange)
 // console.log(merchantBackToMe)
+// // console.log(total - appleOrange)
+// // console.log(apple + orange)
+// // console.log(merchantBackToMe)
+
+
+
+
+
+// again on the class on js 
+console.log(33)
